cf76b77e22c061cc3f715a7b7584cf36672fbcb3
JonyRalf <ralfsntrenje@gmail.com>
2022-10-09 18:41:16 +0300

    Add new line

--- src/main/java/com/example/demotest/HelloApplication.java
+++ src/main/java/com/example/demotest/HelloApplication.java
@@ -16,6 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         System.out.println ("Hello world");
+        System.out.println ("Hello world");
     }
 
     public static void main(String[] args) {
